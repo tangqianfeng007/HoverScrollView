88ce6eff98b54e1010fa31ccc652071660b987bd
唐乾峰 <tangqianfeng@zbj.com>
2019-07-24 08:41:27 +0800

    +++

--- app/src/main/java/com/tqf/demo/hoverscrollview/MainActivity.java
+++ app/src/main/java/com/tqf/demo/hoverscrollview/MainActivity.java
@@ -10,9 +10,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.SparseArray;
 import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * @author tangqianfeng
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout linContain;
     TabLayout tabLayout;
     ViewPager viewPager;
+    FrameLayout header;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +38,34 @@ public class MainActivity extends AppCompatActivity {
         viewPager = findViewById(R.id.vp);
         tabLayout = findViewById(R.id.tablayout);
         hoverScrollView = findViewById(R.id.scrollview);
+        header = findViewById(R.id.header);
         initView();
     }
 
     private void initView() {
+
+        header.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this , "tttttt", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        final int dp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP , 60 , getResources().getDisplayMetrics());
+        hoverScrollView.setonScrollViewListener(new HoverScrollView.ScrollViewListener() {
+            @Override
+            public void onScrollChanged(int x, int y, int oldx, int oldy) {
+                header.setY(Math.max(-y , -dp));
+            }
+        });
+
         for (int i = 0; i < 20; i++) {
             TextView item = new TextView(this);
-            item.setText("item");
+            item.setText("item"+i);
             item.setTextSize(18);
             item.setGravity(Gravity.CENTER);
             item.setPadding(12,12,12,12);
-            linContain.addView(item, 0);
+            linContain.addView(item);
         }
         final HoverpageAdpter hoverpageAdpter = new HoverpageAdpter(getSupportFragmentManager());
         viewPager.setAdapter(hoverpageAdpter);
@@ -69,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
         viewPager.postDelayed(new Runnable() {
             @Override
             public void run() {
-                int tabh = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP , 48 , getResources().getDisplayMetrics());
+                int tabh = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP , 60+48 , getResources().getDisplayMetrics());
                 int vh = hoverScrollView.getHeight() - tabh;
                 LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT , vh);
                 viewPager.setLayoutParams(params);
